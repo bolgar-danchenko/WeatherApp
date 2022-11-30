f1c56c8e428b712b7fda56f915a01d799b2d0441
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2022-11-30 23:33:22 +0400

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Konstantin Bolgar-Danchenko on 30.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
